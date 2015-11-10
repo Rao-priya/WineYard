b5774e1238428089b994399747859175fc27de4f
priyanka <priyankanadagoud@gmail.com>
2015-11-09 19:14:45 -0800

    made change to margin

--- newfilephp.php
+++ newfilephp.php
@@ -5,17 +5,17 @@ $p = "your_password";
 
 
 
-$conn = mysqli_connect('localhost', 'root' ,'',"test1");
-if($conn->connect_error){
+$conn = mysqli_connect('localhost', 'root' ,'',"test1");//get connection
+if($conn->connect_error){//if error in connecting exit
 	die("Connection failed!" .$conn->connect_error);
 }
-else {
+else { ///
 echo "connected yay!!";
-$sql = "SELECT * from users";
-$res = $conn->query($sql);
-if($res->num_rows > 0){
+$sql = "SELECT * from users";//users is table name here
+$res = $conn->query($sql);//res is result of your query
+if($res->num_rows > 0){//res->num of row gives you count of 
 while($row = $res->fetch_assoc()) {
-	echo "id:" .$row["userid"]. " -name:" .$row["username"]. "<br>";
+	echo "id:" .$row["userid"]. " -name:" .$row["username"]. "<br>";//products country, region,
   }//while
 }//if
 else{
