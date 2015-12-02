8dea92c628d5669676c950e75ab7928c09aa7e6d
accShweta <srdeshpande@scu.edu>
2015-12-02 13:26:16 -0800

    Modified the css for text info
    Modified the css for text info

--- html/index.php
+++ html/index.php
@@ -17,13 +17,13 @@ session_start();
          <div class="section">
 		<p id="top">
 
-                    <span style="color: darkred ; font-size:40px; " > Wine Yard </span> is the place for the wine lovers. 
-In our website you can filter the wines based on your preferences such as: wine type, country, region and price. 
+                    <span style="color: darkred ; font-size:24px; " > Wine Yard </span> is the place for the wine lovers. 
+In our website you can filter the wines based on your preferences such as: wine type, country, region and price.
 The purchase process is really simple, just choose your favorite wine and click to purchase! Wine Yard also has a membership club and events agenda.
  Check it out how our membership club works! <br><br>
                   </p>
 
-              
+
 
                     <div class="slideshowbanner">
                       <figure class="show"> <img src="../img/wineslideshow1.jpg" alt="slide1" class="center_img"/> </figure>
@@ -33,7 +33,7 @@ The purchase process is really simple, just choose your favorite wine and click 
                         <span class="prev">&laquo;</span>
                         <span class="next">&raquo;</span>
                     </div>
-          
+
  </div>
 
 
